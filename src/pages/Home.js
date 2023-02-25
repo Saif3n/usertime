3d8067b981a19b842e8d807fcff6821677ba9746
Nicholas <nicholaswu498@gmail.com>
2023-02-25 18:26:00 +1300

    working background with elements on top

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,35 @@ import Granim from "granim";
 function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [granimInstance, setGranimInstance] = useState(null);
 
   useEffect(() => {
-    
+    if (granimInstance === null) {
+      const instance = new Granim({
+        element: "#canvas-basic",
+        direction: "custom",
+        customDirection: {
+          x0: "40%",
+          y0: "10px",
+          x1: "60%",
+          y1: "50%",
+        },
+        isPausedWhenNotInView: true,
+        states: {
+          "default-state": {
+            gradients: [
+              ["#ff9966", "#ff5e62"],
+              ["#00F260", "#0575E6"],
+              ["#e1eec3", "#f05053"],
+            ],
+          },
+        },
+      });
+      setGranimInstance(instance);
+    } else {
+      granimInstance.changeState("default-state");
+    }
+
     const fetchSearchResults = async () => {
       if (searchTerm.trim()) {
         const response = await fetch(
@@ -19,33 +45,10 @@ function Home() {
       }
     };
     fetchSearchResults();
-  }, [searchTerm]);
-
-  new Granim({
-        element: '#canvas-basic',
-        direction: 'custom',
-        customDirection: {
-            x0: '40%',
-            y0: '10px',
-            x1: '60%',
-            y1: '50%',
-        },
-        isPausedWhenNotInView: true,
-        states : {
-            "default-state": {
-                gradients: [
-                    ['#ff9966', '#ff5e62'],
-                    ['#00F260', '#0575E6'],
-                    ['#e1eec3', '#f05053']
-                ]
-            }
-        }
-    });
+  }, [searchTerm, granimInstance]);
 
   return (
     <div>
-    
-
       <div className="search-bar-container">
         <div className="search-bar-wrapper">
           <input
@@ -65,7 +68,7 @@ function Home() {
         </div>
       </div>
       <canvas id="canvas-basic" style={{ zIndex: -1 }}></canvas>
-      </div>
+    </div>
   );
 }
 
