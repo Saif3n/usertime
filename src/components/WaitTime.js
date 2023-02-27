4b1218ae70fc6691d1cefbc612d39e7f07193e2d
Saif3n <nicholaswu498@gmail.com>
2023-02-27 22:43:06 +1300

    adding post request to review

--- src/components/WaitTime.js
+++ src/components/WaitTime.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const WaitTime = React.forwardRef((props, ref) => {
 
+  const submitTime = useRef("");
+
   const [timeWaited, setTimeWaited] = useState(null);
   const [lastReview, setLastReview] = useState(null);
+  const [newReview, setNewReview] = useState(null);
 
   const companyName = props.companyName;
   const companyIndustry = props.companyIndustry;
@@ -20,12 +24,36 @@ const WaitTime = React.forwardRef((props, ref) => {
     fetchReview();
   }, [companyName]);
 
+  const submitReview = () => {
+
+  }
+
   return (
     <div className="companyResult">
       <div className="waitTitle">The last reported wait time for {companyName} was {timeWaited}</div>
       
       <p className="review">Reviewed {lastReview}</p>
       <div className="industry">Industry: {companyIndustry}</div>
+      <hr></hr>
+      <div className="leaveReview">Leave a review</div>
+      <div className="newReview">
+      <div className="bottom">
+      <div className="contact">
+        <h1 className="contactheader">Contact me:</h1>
+
+        <Form onSubmit={submitReview}>
+          <Form.Group>
+            <Form.Control required type="email" placeholder="How long did you wait on the phone?" ref={submitTime} />
+          </Form.Group>
+          <br></br>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </div>
+        </div>
+
+      </div>
     </div>
   );
 });
